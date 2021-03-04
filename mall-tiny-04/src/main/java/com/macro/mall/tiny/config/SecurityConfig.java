e71c40e79a4fa7ed03e0738c95b7e621a370b8e2
zhuzhengDL <zhuzhengahpu@163.com>
2021-03-04 10:58:52 +0800

    放开验证码获取权限

--- mall-tiny-04/src/main/java/com/macro/mall/tiny/config/SecurityConfig.java
+++ mall-tiny-04/src/main/java/com/macro/mall/tiny/config/SecurityConfig.java
@@ -58,7 +58,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         "/**/*.css",
                         "/**/*.js",
                         "/swagger-resources/**",
-                        "/v2/api-docs/**"
+                        "/v2/api-docs/**",
+                        "/sso/**"
                 )
                 .permitAll()
                 .antMatchers("/admin/login", "/admin/register")// 对登录注册要允许匿名访问
